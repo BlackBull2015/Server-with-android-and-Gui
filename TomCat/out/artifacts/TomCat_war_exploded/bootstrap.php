a81fdaa931506acc0eb42ed93b20db039b4a244b
BlackBull2015 <aroslaw007chudy@gmail.com>
2016-04-19 13:53:34 +0100

    unsuccessful make of dynamic graph drawing php page

--- TomCat/out/artifacts/TomCat_war_exploded/bootstrap.php
+++ TomCat/out/artifacts/TomCat_war_exploded/bootstrap.php
@@ -14,13 +14,16 @@
         <script src="amcharts/amcharts.js" type="text/javascript"></script>
 
         <script src="amcharts/serial.js" type="text/javascript"></script>
-    <title>Scrolling Nav - Start Bootstrap Template</title>
+    <title>SSP Bootstrap</title>
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="css/scrolling-nav.css" rel="stylesheet">
+    <!--Footer CSS  and linked in and github     -->
+    <link rel="stylesheet" href="css/footer-distributed-with-address-and-phones.css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -28,202 +31,10 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <?php
+    include 'creategraphs.php';
+    ?>
 
-    <script>
-        var chartData = [];
-        generateChartData();
-
-        var chart1 = AmCharts.makeChart("chartdiv", {
-            type: "serial",
-            dataProvider: chartData,
-            categoryField: "Location",
-            categoryAxis: {
-                //   parseDates: true,
-                gridAlpha: 0.15,
-                minorGridEnabled: true,
-                axisColor: "#DADADA"
-            },
-            valueAxes: [{
-                axisAlpha: 0.2,
-                id: "v1"
-            }],
-            graphs: [{
-                title: "red line",
-                id: "g1",
-                valueAxis: "v1",
-                valueField: "Lin1",
-                //  bullet: "round",
-                bulletBorderColor: "#FFFFFF",
-                bulletBorderAlpha: 1,
-                lineThickness: 2,
-                lineColor: "#b5030d",
-                // negativeLineColor: "#0352b5",
-                balloonText: "[[date]]<br><b><span style='font-size:14px;'>XAxis: [[value]]</span></b>"
-            },{
-                title: "blue line",
-                id: "g2",
-                valueAxis: "v1",
-                valueField: "Lin2",
-                temp:"Lin1",
-                bullet: "round",
-                bulletBorderColor: "#FFFFFF",
-                bulletBorderAlpha: 1,
-                lineThickness: 2,
-                lineColor: "#0352b5",
-                //  negativeLineColor: "#0352b5",
-                balloonText: "[[date]]<br><b><span style='font-size:14px;'>YAxis: [[value]]</span></b><br><b><span style='font-size:14px;'>Temp: [[Lin1]]</span></b>"
-
-            },{
-                title: "blue line",
-                id: "g3",
-                valueAxis: "v1",
-                valueField: "Lin3",
-                bullet: "round",
-                bulletBorderColor: "#FFFFFF",
-                bulletBorderAlpha: 1,
-                lineThickness: 2,
-                lineColor: "#FFFF00",
-                //  negativeLineColor: "#0352b5",
-                balloonText: "[[date]]<br><b><span style='font-size:14px;'>ZAxis: [[value]]</span></b>"
-
-            }],
-            chartCursor: {
-                fullWidth:true,
-                cursorAlpha:0.1
-            },
-            chartScrollbar: {
-                scrollbarHeight: 40,
-                color: "#FFFFFF",
-                autoGridCount: true,
-                graph: "g1",
-                graph: "g2",
-                graph: "g3",
-            },
-
-            mouseWheelZoomEnabled:true
-        });
-
-        var chart2 = AmCharts.makeChart("chartdiv", {
-            type: "serial",
-            dataProvider: chartData,
-            categoryField: "Location",
-            categoryAxis: {
-                //   parseDates: true,
-                gridAlpha: 0.15,
-                minorGridEnabled: true,
-                axisColor: "#DADADA"
-            },
-            valueAxes: [{
-                axisAlpha: 0.2,
-                id: "v1"
-            }],
-            graphs: [{
-                title: "red line",
-                id: "g1",
-                valueAxis: "v1",
-                valueField: "Lin1",
-                //  bullet: "round",
-                bulletBorderColor: "#FFFFFF",
-                bulletBorderAlpha: 1,
-                lineThickness: 2,
-                lineColor: "#b5030d",
-                // negativeLineColor: "#0352b5",
-                balloonText: "[[date]]<br><b><span style='font-size:14px;'>XAxis: [[value]]</span></b>"
-            },{
-                title: "blue line",
-                id: "g2",
-                valueAxis: "v1",
-                valueField: "Lin2",
-                temp:"Lin1",
-                bullet: "round",
-                bulletBorderColor: "#FFFFFF",
-                bulletBorderAlpha: 1,
-                lineThickness: 2,
-                lineColor: "#0352b5",
-                //  negativeLineColor: "#0352b5",
-                balloonText: "[[date]]<br><b><span style='font-size:14px;'>YAxis: [[value]]</span></b><br><b><span style='font-size:14px;'>Temp: [[Lin1]]</span></b>"
-
-            },{
-                title: "blue line",
-                id: "g3",
-                valueAxis: "v1",
-                valueField: "Lin3",
-                bullet: "round",
-                bulletBorderColor: "#FFFFFF",
-                bulletBorderAlpha: 1,
-                lineThickness: 2,
-                lineColor: "#FFFF00",
-                //  negativeLineColor: "#0352b5",
-                balloonText: "[[date]]<br><b><span style='font-size:14px;'>ZAxis: [[value]]</span></b>"
-
-            }],
-            chartCursor: {
-                fullWidth:true,
-                cursorAlpha:0.1
-            },
-            chartScrollbar: {
-                scrollbarHeight: 40,
-                color: "#FFFFFF",
-                autoGridCount: true,
-                graph: "g1",
-                graph: "g2",
-                graph: "g3",
-            },
-
-            mouseWheelZoomEnabled:true
-        });
-
-        chart1.addListener("dataUpdated", zoomChart);
-        chart2.addListener("dataUpdated", zoomChart);
-
-
-        // generate some random data, quite different range
-        function generateChartData() {
-
-            for (var i = 0; i < 500; i++) {
-                // we create date objects here. In your data, you can have date strings
-                // and then set format of your dates using chart.dataDateFormat property,
-                // however when possible, use date objects, as this will speed up chart rendering.
-                var newDate =  AmCharts.stringToDate("2016-04-04-16:47:03", "YYYY-MM-DD-JJ:NN:SS");
-
-                var line1 = Math.round(Math.random() * 40) - 20;
-                var line2 = Math.round(Math.random() * 40) - 20;
-                var line3 = Math.round(Math.random() * 40) - 20;
-                var tmp = Math.round(Math.random() * 40) - 20;
-                var Loc = i;
-
-                chartData.push({
-                    date: newDate,
-                    Lin1: line1,
-                    Lin2: line2,
-                    Lin3: line3,
-                    temp: tmp,
-                    Location: Loc
-                });
-            }
-        }
-
-        // this method is called when chart is first inited as we listen for "dataUpdated" event
-        function zoomChart() {
-            // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
-            chart1.zoomToIndexes(chartData.length - 40, chartData.length - 1);
-            chart2.zoomToIndexes(chartData.length - 40, chartData.length - 1);
-        }
-
-        // changes cursor mode from pan to select
-        function setPanSelect() {
-            var chartCursor = chart.chartCursor;
-
-            if (document.getElementById("rb1").checked) {
-                chartCursor.pan = false;
-                chartCursor.zoomable = true;
-
-            } else {
-                chartCursor.pan = true;
-            }
-            chart.validateNow();
-        }
-    </script>
 </head>
 
 <!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
@@ -240,7 +51,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>
+            <a class="navbar-brand page-scroll" href="#page-top">Welcome To SSP</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
@@ -254,11 +65,19 @@
                     <a class="page-scroll" href="#about">About</a>
                 </li>
                 <li>
-                    <a class="page-scroll" href="#services">Services</a>
+                    <a class="page-scroll" href="#accgrp">Accelerometer graph</a>
                 </li>
                 <li>
-                    <a class="page-scroll" href="#contact">Contact</a>
+                    <a class="page-scroll" href="#maggrp">Magnetometer Graph</a>
                 </li>
+                <li>
+                    <a class="page-scroll" href="#credits">Credits</a>
+                </li>
+                <li>
+                    <a class="page-scroll" href="#Contact">Contact Us</a>
+                </li>
+
+
             </ul>
         </div>
         <!-- /.navbar-collapse -->
@@ -271,9 +90,9 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                <h1>Scrolling Nav</h1>
-                <p><strong>Usage Instructions:</strong> Make sure to include the <code>scrolling-nav.js</code>, <code>jquery.easing.min.js</code>, and <code>scrolling-nav.css</code> files. To make a link smooth scroll to another section on the page, give the link the <code>.page-scroll</code> class and set the link target to a corresponding ID on the page.</p>
-                <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a>
+                <h1>SSP</h1>
+                <p><strong>Innovative</strong> technology in smart sensors field<br><strong>Totally new approach</strong> to communication between sensor pack and servers <br><strong>Welcome</strong> to ssp page</p>
+              <a class="btn btn-default page-scroll" href="#about">Click here to start</a>
             </div>
         </div>
     </div>
@@ -281,43 +100,112 @@
 </section>
 
 <!-- About Section -->
-<section id="about" class="about-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>About Section</h1>
+<section id="about"class="about-section" >
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 style="color: whitesmoke">About Section</h1>
+                    <p style="color: whitesmoke"><strong>Short description:</strong> SSP is a smart sensor pack that use KL26Z and can be mounted on any vehical.<br>By smart communication to servers all data comes live in just a second.<br>SSP is a final year project for Computer and Electronig Engineering in GMIT<br>Most recent data below></p>
+                    <a class="btn btn-default page-scroll" href="#accgrp">Click to proceed</a>
+                </div>
             </div>
         </div>
-    </div>
 </section>
 
 <!-- Services Section -->
-<section id="services" class="services-section">
+<section id="accgrp" class="accgrp-section" >
+                <h1 style="margin-top: 0">Accelerometer Graph</h1>
+                <div id="chart1div" style="width: 100%; height: 400px;"></div>
+                <input type="radio" checked="true" name="group" id="rb1" onclick="setPanSelect()">Select
+                <input type="radio" name="group" id="rb2" onclick="setPanSelect()">Pan</div>
+</section>
+
+<!-- Contact Section -->
+<section id="maggrp" class="maggrp-section">
+
+                <h1 style="margin-top: 0">Magnetometer Graph</h1>
+                <div id="chart2div" style="width: 100%; height: 400px; "></div>
+                <input type="radio" checked="true" name="group" id="rb3" onclick="setPanSelect()">Select
+                <input type="radio" name="group" id="rb4" onclick="setPanSelect()">Pan
+</section>
+
+<!-- Contact Section -->
+<section id="credits" class="credit-section">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
-                              <div id="chart1.write("chart1div");" style="width: 100%; height: 400px;"></div>
-                sadffgfafdas
-                <?php
-                echo "hello there";
-                ?>
-                <h1>Services Section</h1>
+                <h1 >Credits Section</h1>
+                <p>Thank you for visiting my website for SSP project.<br>Please come back to see more data</p>
+
             </div>
         </div>
     </div>
 </section>
 
-<!-- Contact Section -->
-<section id="contact" class="contact-section">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div id="chart2.write("chart2div");" style="width: 100%; height: 400px;"></div>
-                <h1>Contact Section</h1>
-            </div>
+<section id="Contact" class="contact-section">
+    <footer class="footer-distributed">
+
+        <div class="footer-left">
+
+            <h3>SSP<span>-Smart Sensor Pack</span></h3>
+
+            <p class="footer-links">
+                <a href="#intro">Home</a>-
+                <a href="#about">About</a>-
+                <a href="#accgrp">Graph1</a>-
+                <a href="#maggrp">Graph2</a>-
+                <a href="#credits">Credits</a>-
+                <a href="#Contact">Contact</a>
+            </p>
+
+            <p class="footer-company-name">Arkadiusz Bochen &copy; 2016</p>
+
+            <p>Templates used<br><a href="http://startbootstrap.com/">Main template<br><a href="http://tutorialzine.com/2015/01/freebie-5-responsive-footer-templates/">Footer tempate</a> </a> </p>
+
         </div>
-    </div>
+
+        <div class="footer-center">
+
+            <div>
+                <i class="fa fa-map-marker"></i>
+                <p><span>konckatee West, Dunmore</span> Galway, Ireland</p>
+            </div>
+
+            <div>
+                <i class="fa fa-phone"></i>
+                <p>+353 86 3249 640</p>
+            </div>
+
+            <div>
+                <i class="fa fa-envelope"></i>
+                <p><a href="mailto:bochenarkadiusz@gmail.com">bochenarkadiusz@gmail.com</a></p>
+            </div>
+
+        </div>
+
+        <div class="footer-right">
+
+            <p class="footer-company-about">
+                <span>About project</span>
+                SSp is a smart sensor pack that can be delivered in varoius forms and be conneced by different technologies. Also it can be installes on any vehicle
+            </p>
+
+            <div class="footer-icons">
+
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="https://ie.linkedin.com/in/arkadiusz-bochen-3346b180"><i class="fa fa-linkedin"></i></a>
+                <a href="https://github.com/BlackBull2015"><i class="fa fa-github"></i></a>
+
+            </div>
+
+        </div>
+
+    </footer>
 </section>
+
+
+
 
 <!-- jQuery -->
 <script src="js/jquery.js"></script>
@@ -328,7 +216,6 @@
 <!-- Scrolling Nav JavaScript -->
 <script src="js/jquery.easing.min.js"></script>
 <script src="js/scrolling-nav.js"></script>
-
 </body>
 
 </html>
---
+++ TomCat/out/artifacts/TomCat_war_exploded/creategraphs.php
@@ -0,0 +1,297 @@
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <script src="amcharts/amcharts.js" type="text/javascript"></script>
+    <script src="amcharts/serial.js" type="text/javascript"></script>
+    <script>
+        <?php
+      $servername = "localhost:3306";
+      $username = "root";
+      $password = "root";
+      $dbname = "readings";
+
+      $conn = new mysqli("localhost:3306", "root", "root",$dbname);
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+      }
+
+      $sql = "SELECT * FROM datareadings";
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+          // output data of each row
+
+          $accX = "[";
+          $accY = "[";
+          $accZ = "[";
+          $magX = "[";
+          $magY = "[";
+          $magZ = "[";
+          $tmp = "[";
+          $time = "[";
+          $size = 0;
+          while($row = $result->fetch_assoc()) {
+              $accX .= $row["accX"].", ";
+              $accY .= $row["accY"].", ";
+              $accZ .= $row["accZ"].", ";
+              $magX .= $row["magX"].", ";
+              $magY .= $row["magY"].", ";
+              $magZ .= $row["magZ"].", ";
+              $tmp .= $row["tmp"].", ";
+              $time .= "\"".$row["time"]."\", ";
+              $size = $row["tableID"];
+          }
+          $accX .= "]";
+          $accY .= "]";
+          $accZ .= "]";
+          $magX .= "]";
+          $magY .= "]";
+          $magZ .= "]";
+          $tmp .= "]";
+          $time .= "]";
+
+      } else {
+          echo "0 results";
+      }
+      $conn->close();
+      ?>
+        var chartData = [];
+        var chartData2 = [];
+        generateChartData();
+        generateChartData2();
+
+
+        var chart1 = AmCharts.makeChart("chart1div", {
+            type: "serial",
+            dataProvider: chartData,
+            categoryField: "Location1",
+            categoryAxis: {
+                gridAlpha: 0.15,
+                minorGridEnabled: true,
+                axisColor: "#DADADA"
+            },
+            valueAxes: [{
+                axisAlpha: 0.2,
+                id: "v1"
+            }],
+            graphs: [{
+                title: "red line",
+                id: "g1",
+                valueAxis: "v1",
+                valueField: "Lin1",
+                //  bullet: "round",
+                bulletBorderColor: "#FFFFFF",
+                bulletBorderAlpha: 1,
+                lineThickness: 2,
+                lineColor: "#b5030d",
+                // negativeLineColor: "#0352b5",
+                balloonText: "[[date]]<br><b><span style='font-size:14px;'>AccXAxis: [[value]]</span></b>"
+            },{
+                title: "blue line",
+                id: "g2",
+                valueAxis: "v1",
+                valueField: "Lin2",
+                temp:"Lin1",
+                bullet: "round",
+                bulletBorderColor: "#FFFFFF",
+                bulletBorderAlpha: 1,
+                lineThickness: 2,
+                lineColor: "#0352b5",
+                //  negativeLineColor: "#0352b5",
+                balloonText: "[[date]]<br><b><span style='font-size:14px;'>AccYAxis: [[value]]</span></b><br><b><span style='font-size:14px;'>Temp: [[temp]]</span></b>"
+
+            },{
+                title: "blue line",
+                id: "g3",
+                valueAxis: "v1",
+                valueField: "Lin3",
+                bullet: "round",
+                bulletBorderColor: "#FFFFFF",
+                bulletBorderAlpha: 1,
+                lineThickness: 2,
+                lineColor: "#FFFF00",
+                //  negativeLineColor: "#0352b5",
+                balloonText: "[[date]]<br><b><span style='font-size:14px;'>AccZAxis: [[value]]</span></b>"
+
+            }],
+            chartCursor: {
+                fullWidth:true,
+                cursorAlpha:0.1
+            },
+            chartScrollbar: {
+                scrollbarHeight: 40,
+                color: "#FFFFFF",
+                autoGridCount: true,
+                graph: "g1",
+                graph: "g2",
+                graph: "g3",
+            },
+
+            mouseWheelZoomEnabled:true
+        });
+
+        var chart2 = AmCharts.makeChart("chart2div", {
+            type: "serial",
+            dataProvider: chartData2,
+            categoryField: "Location2",
+            categoryAxis: {
+                gridAlpha: 0.15,
+                minorGridEnabled: true,
+                axisColor: "#DADADA"
+            },
+            valueAxes: [{
+                axisAlpha: 0.2,
+                id: "v1"
+            }],
+            graphs: [{
+                title: "red line",
+                id: "g1",
+                valueAxis: "v1",
+                valueField: "Lin1",
+                //  bullet: "round",
+                bulletBorderColor: "#FFFFFF",
+                bulletBorderAlpha: 1,
+                lineThickness: 2,
+                lineColor: "#b5030d",
+                // negativeLineColor: "#0352b5",
+                balloonText: "[[date]]<br><b><span style='font-size:14px;'>MagXAxis: [[value]]</span></b>"
+            },{
+                title: "blue line",
+                id: "g2",
+                valueAxis: "v1",
+                valueField: "Lin2",
+                temp:"Lin1",
+                bullet: "round",
+                bulletBorderColor: "#FFFFFF",
+                bulletBorderAlpha: 1,
+                lineThickness: 2,
+                lineColor: "#0352b5",
+                //  negativeLineColor: "#0352b5",
+                balloonText: "[[date]]<br><b><span style='font-size:14px;'>MagYAxis: [[value]]</span></b><br><b><span style='font-size:14px;'>Temp: [[temp]]</span></b>"
+
+            },{
+                title: "blue line",
+                id: "g3",
+                valueAxis: "v1",
+                valueField: "Lin3",
+                bullet: "round",
+                bulletBorderColor: "#FFFFFF",
+                bulletBorderAlpha: 1,
+                lineThickness: 2,
+                lineColor: "#FFFF00",
+                //  negativeLineColor: "#0352b5",
+                balloonText: "[[date]]<br><b><span style='font-size:14px;'>MagZAxis: [[value]]</span></b>"
+
+            }],
+            chartCursor: {
+                fullWidth:true,
+                cursorAlpha:0.1
+            },
+            chartScrollbar: {
+                scrollbarHeight: 40,
+                color: "#FFFFFF",
+                autoGridCount: true,
+                graph: "g1",
+                graph: "g2",
+                graph: "g3",
+            },
+
+            mouseWheelZoomEnabled:true
+        });
+
+        chart1.addListener("dataUpdated", zoomChart);
+        chart2.addListener("dataUpdated", zoomChart);
+
+
+        // generate some random data, quite different range
+        function generateChartData() {
+
+
+            var firstDate = new Date();
+            var ArrayX = <?php echo $accX?>;
+            var ArrayY =<?php echo $accY?>;
+            var ArrayZ =<?php echo $accZ?>;
+            var Temp = <?php echo $tmp ?>;
+            var Time = <?php echo $time?>;
+            for (var i = 0; i < <?php echo $size?>; i++) {
+                // we create date objects here. In your data, you can have date strings
+                // and then set format of your dates using chart.dataDateFormat property,
+                // however when possible, use date objects, as this will speed up chart rendering.
+                var newDate =  AmCharts.stringToDate(Time[i], "YYYY-MM-DD-JJ:NN:SS");;
+
+                var line1 = ArrayX[i];
+                var line2 = ArrayY[i];
+                var line3 = ArrayZ[i];
+                var tmp = Temp[i];
+                var Loc = i;
+
+                chartData.push({
+                    date: newDate,
+                    Lin1: line1,
+                    Lin2: line2,
+                    Lin3: line3,
+                    temp: tmp,
+                    Location1: Loc
+                });
+            }
+        }
+        // generate some random data, quite different range
+        function generateChartData2() {
+
+            var firstDate = new Date();
+            var ArrayX = <?php echo $magX?>;
+            var ArrayY =<?php echo $magY?>;
+            var ArrayZ =<?php echo $magZ?>;
+            var Temp = <?php echo $tmp ?>;
+            var Time = <?php echo $time?>;
+            for (var i = 0; i < <?php echo $size?>; i++) {
+                // we create date objects here. In your data, you can have date strings
+                // and then set format of your dates using chart.dataDateFormat property,
+                // however when possible, use date objects, as this will speed up chart rendering.
+                var newDate =  AmCharts.stringToDate(Time[i], "YYYY-MM-DD-JJ:NN:SS");;
+
+                var line1 = ArrayX[i];
+                var line2 = ArrayY[i];
+                var line3 = ArrayZ[i];
+                var tmp = Temp[i];
+                var Loc = i;
+
+                chartData2.push({
+                    date: newDate,
+                    Lin1: line1,
+                    Lin2: line2,
+                    Lin3: line3,
+                    temp: tmp,
+                    Location2: Loc
+                });
+            }
+        }
+        // this method is called when chart is first inited as we listen for "dataUpdated" event
+        function zoomChart() {
+            // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
+            chart1.zoomToIndexes(chartData.length - 40, chartData.length - 1);
+            chart2.zoomToIndexes(chartData2.length - 40, chartData2.length - 1);
+        }
+
+        // changes cursor mode from pan to select
+        function setPanSelect() {
+            var chartCursor = chart1.chartCursor;
+            var chartCursor2 = chart2.chartCursor;
+
+            if (document.getElementById("rb1").checked) {
+                chartCursor.pan = false;
+                chartCursor.zoomable = true;
+
+            } else {
+                chartCursor.pan = true;
+            }
+            if (document.getElementById("rb3").checked) {
+                chartCursor2.pan = false;
+                chartCursor2.zoomable = true;
+
+            } else {
+                chartCursor2.pan = true;
+            }
+            chart1.validateNow();
+            chart2.validateNow();
+        }
+
+    </script>
